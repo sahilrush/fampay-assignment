1fe3c33459397b55c29a8d2e8fae730285f297c3
sahilrush <sahilrevankar20co21@gmail.com>
2025-01-19 23:15:30 +0530

    youtubeService completed

--- src/services/youtubeServices.go
+++ src/services/youtubeServices.go
@@ -1,15 +1,42 @@
 package services
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"time"
+
+	"github.com/sahilrush/src/models"
+	"gorm.io/gorm"
 )
 
+type YoutubeService struct {
+	DB *gorm.DB
+}
+
+type ApiResponse struct {
+	Items []struct {
+		Snippet struct {
+			Title       string `json:"title"`
+			Description string `json:"description"`
+			PublishedAt string `json:"publishedAt"`
+			Thumbnails  struct {
+				High struct {
+					URL string `json:"url"`
+				} `json:"high"`
+			} `json:"thumbnails"`
+		} `json:"snippet"`
+	} `json:"items"`
+}
+
+// FetchVideos fetches the latest Youtube videos and stores them
 func (services *YoutubeService) FetchVideos(query string) {
 	client := &http.Client{}
 	url := "https://www.googleapis.com/youtube/v3/search"
+	apiKey := os.Getenv("YOUTUBE_API_KEY")
 
 	for {
 		req, err := http.NewRequest("GET", url, nil)
@@ -18,6 +45,8 @@ func (services *YoutubeService) FetchVideos(query string) {
 			time.Sleep(10 * time.Second)
 			continue
 		}
+
+		//adding query parameter
 		q := req.URL.Query()
 		q.Add("part", "snippet")
 		q.Add("q", query)
@@ -29,7 +58,37 @@ func (services *YoutubeService) FetchVideos(query string) {
 
 		resp, err := client.Do(req)
 		if err != nil {
-			fmt.Println("Error ")
+			fmt.Println("Error making youtube api request:", err)
+			time.Sleep(10 * time.Second)
+			continue
 		}
+		defer resp.Body.Close()
+
+		//parse the response
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("error reading response Body", err)
+			time.Sleep(10 * time.Second)
+			continue
+		}
+
+		var apiResp ApiResponse
+		if err := json.Unmarshal(body, &apiResp); err != nil {
+			fmt.Println("error unmarshalling response", err)
+			continue
+		}
+
+		//save videos to database
+		for _, item := range apiResp.Items {
+			publishedAt, _ := time.Parse(time.RFC3339, item.Snippet.PublishedAt)
+			videos := models.Video{
+				Title:       item.Snippet.Title,
+				Description: item.Snippet.Description,
+				PublishedAt: publishedAt,
+				Thumbnails:  item.Snippet.Thumbnails.High.URL,
+			}
+			services.DB.Create(&videos)
+		}
+		time.Sleep(10 * time.Second)
 	}
 }
